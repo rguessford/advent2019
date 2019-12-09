f832c025c3698f86b7b0a1bb967229a21f4ea4f0
rguessford <rguessford@github.com>
2019-12-09 14:29:46 -0600

    Revert "day 9, now with 38% less code"
    This reverts commit 2807802

--- src/main/java/day9.java
+++ src/main/java/day9.java
@@ -10,10 +10,6 @@ public class day9 {
     public static final int PARAMETER_MODE_2 = 1;
     public static final int PARAMETER_MODE_3 = 0;
 
-    public static final int PARAMETER_1 = 1;
-    public static final int PARAMETER_2 = 2;
-    public static final int PARAMETER_3 = 3;
-
     public static final int POSITION_MODE = 0;
     public static final int IMMEDIATE_MODE = 1;
     public static final int RELATIVE_MODE = 2;
@@ -34,57 +30,167 @@ public class day9 {
 
                 if(instruction[OPCODE] == 1) {
                     //add
-                    long a = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
-                    long b = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
-                    writeValue(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_3, a+b);
+                    long a = 0;
+                    long b = 0;
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        a = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        a = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        a = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                        b = memory[(int) memory[executionPoint + 2]];
+                    } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE){
+                        b = memory[executionPoint + 2];
+                    } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                        b = memory[(int) memory[executionPoint + 2] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_3] == POSITION_MODE) {
+                        memory[(int) memory[executionPoint + 3]] = a + b;
+                    } else if (instruction[PARAMETER_MODE_3] == RELATIVE_MODE) {
+                        memory[(int) memory[executionPoint + 3] + (int) relativeBase] = a + b;
+                    }
                     executionPoint +=4;
                 } else if (instruction[OPCODE] == 2) {
                     //multiply
-                    long a = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
-                    long b = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
-                    writeValue(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_3, a*b);
+                    long a = 0;
+                    long b = 0;
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        a = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        a = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        a = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                        b = memory[(int) memory[executionPoint + 2]];
+                    } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE){
+                        b = memory[executionPoint + 2];
+                    } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                        b = memory[(int) memory[executionPoint + 2] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_3] == POSITION_MODE) {
+                        memory[(int) memory[executionPoint + 3]] = a * b;
+                    } else if (instruction[PARAMETER_MODE_3] == RELATIVE_MODE) {
+                        memory[(int) memory[executionPoint + 3] + (int) relativeBase] = a * b;
+                    }
                     executionPoint+=4;
                 } else if (instruction[OPCODE] == 3) {
                     //input
                     long in = scanner.nextInt();
-                    writeValue(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1, in);
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        memory[(int) memory[executionPoint + 1]] = in;
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        memory[(int) memory[executionPoint + 1] + (int) relativeBase] = in;
+                    }
                     executionPoint+=2;
                 } else if (instruction[OPCODE] == 4) {
                     //output
-                    long a = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
+                    long a = 0;
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        a = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        a = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        a = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
                     System.out.println(a);
                     executionPoint+=2;
                 } else if (instruction[OPCODE] == 5) {
                     //jump if param 1 != 0
-                    long test = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
+                    long test = 0;
+                    if (instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        test = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE) {
+                        test = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        test = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
                     if(test == 0) {
                         executionPoint += 3;
-                    } else {
-                        executionPoint = (int) readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
+                    } else if (instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                        executionPoint = (int) memory[(int) memory[executionPoint + 2]];
+                    } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE) {
+                        executionPoint = (int) memory[executionPoint + 2];
+                    } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                        executionPoint = (int) memory[(int) memory[executionPoint + 2] + (int) relativeBase];
                     }
                 } else if (instruction[OPCODE] == 6) {
                     //jump if param 1 == 0
-                    long test = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
-                    if(test != 0) {
-                        executionPoint += 3;
-                    } else {
-                        executionPoint = (int) readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
+                    long test = 0;
+                    if (instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        test = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE) {
+                        test = memory[executionPoint + 1];
+                    }  else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        test = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
                     }
+                    if(test == 0) {
+                        if (instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                            executionPoint = (int) memory[(int) memory[executionPoint + 2]];
+                        } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE) {
+                            executionPoint = (int) memory[executionPoint + 2];
+                        } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                            executionPoint = (int) memory[(int) memory[executionPoint + 2] + (int) relativeBase];
+                        }
+                    } else { executionPoint += 3;}
                 } else if (instruction[OPCODE] == 7) {
                     //less than
-                    long a = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
-                    long b = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
-                    writeValue(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_3, a < b ? 1 : 0);
+                    long a = 0;
+                    long b = 0;
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        a = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        a = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        a = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                        b = memory[(int) memory[executionPoint + 2]];
+                    } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE){
+                        b = memory[executionPoint + 2];
+                    } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                        b = memory[(int) memory[executionPoint + 2] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_3] == POSITION_MODE) {
+                        memory[(int) memory[executionPoint + 3]] = a < b ? 1 : 0;
+                    } else if (instruction[PARAMETER_MODE_3] == RELATIVE_MODE) {
+                        memory[(int) memory[executionPoint + 3] + (int) relativeBase] = a < b ? 1 : 0;
+                    }
                     executionPoint+=4;
                 } else if (instruction[OPCODE] == 8) {
-                    //equals
-                    long a = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
-                    long b = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_2);
-                    writeValue(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_3, a == b ? 1 : 0);
+                    long a = 0;
+                    long b = 0;
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        a = memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        a = memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE) {
+                        a = memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_2] == POSITION_MODE) {
+                        b = memory[(int) memory[executionPoint + 2]];
+                    } else if (instruction[PARAMETER_MODE_2] == IMMEDIATE_MODE){
+                        b = memory[executionPoint + 2];
+                    } else if (instruction[PARAMETER_MODE_2] == RELATIVE_MODE) {
+                        b = memory[(int) memory[executionPoint + 2] + (int) relativeBase];
+                    }
+                    if(instruction[PARAMETER_MODE_3] == POSITION_MODE) {
+                        memory[(int) memory[executionPoint + 3]] = a == b ? 1 : 0;
+                    } else if (instruction[PARAMETER_MODE_3] == RELATIVE_MODE) {
+                        memory[(int) memory[executionPoint + 3] + (int) relativeBase] = a == b ? 1 : 0;
+                    }
                     executionPoint+=4;
                 } else if (instruction[OPCODE] == 9) {
                     //adjust relative base
-                    relativeBase = readParameter(memory, (int) relativeBase, executionPoint, instruction, PARAMETER_1);
+                    if(instruction[PARAMETER_MODE_1] == POSITION_MODE) {
+                        relativeBase += memory[(int) memory[executionPoint + 1]];
+                    } else if (instruction[PARAMETER_MODE_1] == IMMEDIATE_MODE){
+                        relativeBase += memory[executionPoint + 1];
+                    } else if (instruction[PARAMETER_MODE_1] == RELATIVE_MODE){
+                        relativeBase +=memory[(int) memory[executionPoint + 1] + (int) relativeBase];
+                    }
                     executionPoint +=2;
 
                 } else if (instruction[OPCODE] == 99) break;
@@ -94,33 +200,5 @@ public class day9 {
         } catch(IOException e) {
             System.err.println(e.getMessage());
         }
-    }
-
-    private static void writeValue(long[] memory, int relativeBase, int executionPoint, long[] instruction, int addressParameter, long value) {
-        int parameterMode = -1;
-        if (addressParameter == PARAMETER_1) parameterMode = PARAMETER_MODE_1;
-        if (addressParameter == PARAMETER_2) parameterMode = PARAMETER_MODE_2;
-        if (addressParameter == PARAMETER_3) parameterMode = PARAMETER_MODE_3;
-
-        if (instruction[parameterMode] == POSITION_MODE) {
-            memory[(int) memory[executionPoint + addressParameter]] = value;
-        } else if (instruction[parameterMode] == RELATIVE_MODE) {
-            memory[(int) memory[executionPoint + addressParameter] + relativeBase] = value;
-        }
-    }
-
-    private static long readParameter(long[] memory, int relativeBase, int executionPoint, long[] instruction, int parameter) {
-        int parameterMode = -1;
-        if (parameter == PARAMETER_1) parameterMode = PARAMETER_MODE_1;
-        if (parameter == PARAMETER_2) parameterMode = PARAMETER_MODE_2;
-        long value = 0;
-        if (instruction[parameterMode] == POSITION_MODE) {
-            value = memory[(int) memory[executionPoint + parameter]];
-        } else if (instruction[parameterMode] == IMMEDIATE_MODE) {
-            value = memory[executionPoint + parameter];
-        } else if (instruction[parameterMode] == RELATIVE_MODE) {
-            value = memory[(int) memory[executionPoint + parameter] + relativeBase];
-        }
-        return value;
     }
 }
